27d6c0bccd363b6f2a94e083b49aafbfdc6b7069
dongdongdongk <dhk9309@gmail.com>
2023-11-18 23:13:57 +0900

    테스트 코드 작성

--- src/test/java/org/zerock/jpaboard/repository/BoardRepositoryTests.java
+++ src/test/java/org/zerock/jpaboard/repository/BoardRepositoryTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.jpaboard.entity.Board;
 import org.zerock.jpaboard.entity.Member;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -27,6 +28,16 @@ public class BoardRepositoryTests {
                     .build();
             boardRepository.save(board);
         });
+    }
+
+    @Test
+    public void testRead1() {
+        Optional<Board> result = boardRepository.findById(100L); // 데이터베이스에 존재하는 번호
+
+        Board board = result.get();
+
+        System.out.println(board);
+        System.out.println(board.getWriter());
     }
 
 }
